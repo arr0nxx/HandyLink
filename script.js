42b78304e07d2e031e2eadd14418b4064d75014d
arr0nxx <cortezjake12@gmail.com>
2025-03-23 11:12:27 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// You can add interactivity here, such as form validation or dynamic content loading
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('HandyLink is ready!');
+});
